b532f09ec3d224042114486f3d0e86029e25b2bc
rancho83a <rancho83a@gmail.com>
2021-12-02 01:44:58 +0200

    second Unit Test

--- src/test/java/forex/copytradingforex/service/impl/CopyTradingForexUserDetailsServiceImplTest.java
+++ src/test/java/forex/copytradingforex/service/impl/CopyTradingForexUserDetailsServiceImplTest.java
@@ -9,17 +9,21 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class CopyTradingForexUserDetailsServiceImplTest {
 
     private CopyTradingForexUserDetailsServiceImpl serviceToTest;
+    private UserEntity testUser;
     @Mock
     private UserRepository mockUserRepository;
 
@@ -32,20 +36,45 @@ class CopyTradingForexUserDetailsServiceImplTest {
         RoleEntity trader = new RoleEntity().setRole(RoleEnum.TRADER);
         RoleEntity investor = new RoleEntity().setRole(RoleEnum.INVESTOR);
 
-        UserEntity testUser = new UserEntity();
+        testUser = new UserEntity();
         testUser.setUsername("master")
+                .setPassword("12345")
                 .setEmail("master@copytradingforex.com")
                 .setRoles(Set.of(master, trader, investor));
     }
     @Test
     void testUsernameNotFoundException(){
+
         Assertions.assertThrows(
                 UsernameNotFoundException.class,
                 ()->serviceToTest.loadUserByUsername("invalid_username")
         );
+    }
 
+    @Test
+    void testUsernameFound(){
+        //Arrange
+
+        Mockito.when(mockUserRepository.findByUsername(testUser.getUsername()))
+                .thenReturn(Optional.of(testUser));
+
+        //Act
+        var actual = serviceToTest.loadUserByUsername(testUser.getUsername());
+
+
+        //Assert
+        Assertions.assertEquals(actual.getUsername(), testUser.getUsername());
+
+        String actualRoles = actual.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(", "));
+
+        String expected = "ROLE_INVESTOR, ROLE_MASTER, ROLE_TRADER";
+
+        Assertions.assertEquals(actualRoles, expected);
 
     }
+
 
 
 }
